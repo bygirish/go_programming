4fea2849ce89099b1c0436f52dffa24f3b80d782
bygirish <girishkumargupta3@gmail.com>
2021-05-13 08:05:44 +0530

    application structure and variable basics added

---
+++ application_structure/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+const secondsInHour = 3600
+
+func main() {
+	fmt.Println("Hello Go world")
+
+	distance := 60.8
+
+	fmt.Printf("The distance in miles is %f \n", distance/0.62137)
+
+}
